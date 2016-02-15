40abc7d07ed61069dce1d4c72506a99f1d62bbcb
jianwen <jianwen@pipapai.com>
2016-02-15 14:50:16 +0800

    增加文件MD5校验，只提供文件下载
    将文件移出代码目录，增加MD5校验并且只提供下载

--- prj_detail.php
+++ prj_detail.php
@@ -22,17 +22,34 @@ $prj_title = mysqli_fetch_array($result1);
 <tr>
 <th>应用平台</th>
 <th>&nbsp;文件名&nbsp;</th>
+<th>&nbsp;MD5&nbsp;</th>
+<th>上传人</th>
 <th>上传时间</th>
-<th>&nbsp;安装&nbsp;</th>
+<th>&nbsp;下载&nbsp;</th>
 </tr>
 <?php
-    $sql="select distinct app_name,platform,add_time from new_app_info where project=$prj and app_status=1 and app_power=1  ORDER BY platform asc ";
+    $sql="select distinct app_name,platform,add_time,add_name from new_app_info where project=$prj and app_status=1 and app_power=1  ORDER BY platform asc ";
   @  $result1=mysqli_query($conn,$sql);
  while($row = mysqli_fetch_array($result1)){
 //while($row = mysqli_fetch_assoc($result1)){
     $v=$row[app_name];
+    $user=$row[add_name];
     $ex=explode('.',$v);
     $num=count($ex);
+    if($ex[$num-1]=="ipa"){
+        // $ServerUrl=$_SERVER['DOCUMENT_ROOT'];
+        // $su=array_pop($ex);
+        // $str = implode('.',$ex);
+        // $ipa= "itms-services://?action=download-manifest&url=https:localhost/AppShop/new_app/pub_plist/";
+        // $plist=$str.".plist";
+        // $down=$ipa.$plist;
+        $down="../new_app/pub_ipa/".$v;
+
+    }
+    else{
+        $down="../new_app/pub_apk/".$v;
+    }
+    $md5file = md5_file($down);
     if($ex[0]!=''){
                 ?>
             <tr>
@@ -41,27 +58,16 @@ $prj_title = mysqli_fetch_array($result1);
                 <td style="line-height:200%" ><?php echo mb_substr($v,0,3,"utf-8")."..."; ?></td>
                 -->
                 <td style="line-height:200%" ><?php echo $v; ?></td>
+                <td style="line-height:200%" ><?php echo $md5file; ?></td>
+                <td style="line-height:200%" ><?php echo $user; ?></td>
                 <td style="line-height:200%" ><?php echo mb_substr($row[add_time],0,10,"utf-8"); ?></td>
-                <?php
-                if($ex[$num-1]=="ipa"){
-                    $ServerUrl=$_SERVER['DOCUMENT_ROOT'];
-                    $su=array_pop($ex);
-                    $str = implode('.',$ex);
-                    $ipa= "itms-services://?action=download-manifest&url=https:localhost/AppShop/new_app/pub_plist/";
-                    $plist=$str.".plist";
-                    $down=$ipa.$plist;
-
-                }
-                else{
-                    $down="new_app/pub_apk/".$v;
-                }
-                ?>
+                
 <!--
 //                <td style="line-height:200%" ><a href="itms-services://?action=download-manifest&url=http:192.168.21.13/plist/<?php //echo $str ?>.plist">安装</a></td>
 
 //                <td style="line-height:200%" ><a href="auto-adroidfile/<?php echo $v ?>">安装</a></td>
  -->
-                <td style="line-height:200%" ><a href="<?php echo $down ?>">安装</a></td>
+                <td style="line-height:200%" ><a href="<?php echo $down ?>">下载</a></td>
             </tr>
 
             <?php
